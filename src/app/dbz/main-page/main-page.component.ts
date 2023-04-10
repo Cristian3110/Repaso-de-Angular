42223e55bec8414a1d47b1ca02ad70176079021e
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2023-04-09 21:54:00 -0400

    refresh ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent {
+  nuevo: Personaje = {
+    nombre: 'Trunk',
+    poder: 15000,
+  };
+
+  cambiarNombre(event: any) {
+    console.log(event.target.value);
+  }
+
   agregar() {
-    console.log('Prueba');
+    console.log(this.nuevo);
   }
 }
